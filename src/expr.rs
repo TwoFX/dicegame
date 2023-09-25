6e5f72944e8fbd8cdf220c5d820a6b82944ad13f
Markus Himmel <markus@himmel-villmar.de>
2023-09-25 21:44:45 +0200

    Evaluation

--- src/expr.rs
+++ src/expr.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use num_rational::Rational64;
+
 /// The basic (binary) operators which are allowed in the dice game.
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub enum Operator {
@@ -27,6 +29,11 @@ pub enum ParseError {
     UnexpectedEnd,
 }
 
+#[derive(PartialEq, Eq, Debug)]
+pub enum EvalError {
+    DivisionByZero,
+}
+
 mod lex;
 mod parse;
 
@@ -49,6 +56,25 @@ impl Expr {
         let mut res = HashMap::new();
         inner(self, &mut res);
         res
+    }
+
+    pub fn eval(&self) -> Result<Rational64, EvalError> {
+        match self {
+            Expr::Num(x) => Ok(Rational64::from_integer(i64::from(*x))),
+            Expr::Op { op, left, right } => {
+                let lhs = left.eval()?;
+                let rhs = right.eval()?;
+                match op {
+                    Operator::Div if rhs == Rational64::from_integer(0) => {
+                        Err(EvalError::DivisionByZero)
+                    }
+                    Operator::Div => Ok(lhs / rhs),
+                    Operator::Add => Ok(lhs + rhs),
+                    Operator::Mul => Ok(lhs * rhs),
+                    Operator::Sub => Ok(lhs - rhs),
+                }
+            }
+        }
     }
 }
 
@@ -73,5 +99,33 @@ mod tests {
         expected.insert(5, 1);
 
         assert_eq!(expected, e.histogram());
+    }
+
+    #[test]
+    fn basic_eval() {
+        let e = Expr::Op {
+            op: Operator::Add,
+            left: Box::new(Expr::Num(6)),
+            right: Box::new(Expr::Op {
+                op: Operator::Div,
+                left: Box::new(Expr::Num(5)),
+                right: Box::new(Expr::Num(6)),
+            }),
+        };
+        assert_eq!(Ok(Rational64::new(41, 6)), e.eval());
+    }
+
+    #[test]
+    fn eval_div_by_zero() {
+        let e = Expr::Op {
+            op: Operator::Div,
+            left: Box::new(Expr::Num(1)),
+            right: Box::new(Expr::Op {
+                op: Operator::Sub,
+                left: Box::new(Expr::Num(1)),
+                right: Box::new(Expr::Num(1)),
+            }),
+        };
+        assert_eq!(Err(EvalError::DivisionByZero), e.eval());
     }
 }

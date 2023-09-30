3e92373b1b229988e8f1f6fdaac0da97b04df770
Markus Himmel <markus@himmel-villmar.de>
2023-09-30 15:43:24 +0200

    Proper implementation of game loop

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,80 @@
+use dicegame::expr;
 use dicegame::expr::Expr;
 use num_rational::Rational64;
 use std::{collections::HashMap, io};
+use thiserror::Error;
 
 fn main() {
     let v = vec![1, 2, 3, 4];
+    play_round(v.as_slice());
+}
+
+#[derive(Debug, Error)]
+enum InputError {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    Parse(#[from] expr::ParseError),
+    #[error(transparent)]
+    Eval(#[from] expr::EvalError),
+    #[error("incorrect histogram: expected {expected:?}, got {actual:?}")]
+    Histogram {
+        expected: HashMap<u32, u32>,
+        actual: HashMap<u32, u32>,
+    },
+    #[error("incorrect value: expected {expected}, got {actual}")]
+    Value {
+        expected: Rational64,
+        actual: Rational64,
+    },
+}
+
+fn play_round(v: &[u32]) {
     println!("{v:?}");
 
+    let expected_histogram = histogram(v);
+    let expected_value = Rational64::from_integer(1);
+
+    loop {
+        if let Err(e) = process_input(&expected_histogram, &expected_value) {
+            println!("Incorrect input: {e}");
+            continue;
+        } else {
+            println!("Correct!");
+            break;
+        }
+    }
+}
+
+fn process_input(
+    expected_histogram: &HashMap<u32, u32>,
+    expected_value: &Rational64,
+) -> Result<(), InputError> {
     let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
 
-    let h = histogram(v.as_slice());
-
-    let parsed = match Expr::parse(input.as_str()) {
-        Ok(p) => p,
-        Err(e) => {
-            println!("Failed to parse: {e}");
-            return;
-        }
-    };
-
-    let hh = parsed.histogram();
-    if h != hh {
-        println!("Incorrect histogram: {hh:?}");
-        return;
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        return Err(InputError::Io(e));
     }
 
-    let one = Rational64::from_integer(1);
-    let pe = match parsed.eval() {
-        Ok(p) => p,
-        Err(e) => {
-            println!("Failed to evaluate: {e}");
-            return;
-        }
-    };
-    if one != pe {
-        println!("Incorrect value: {pe:?}");
-    } else {
-        println!("Correct!!");
+    let expr = Expr::parse(input.as_str()).map_err(InputError::Parse)?;
+
+    let actual_histogram = expr.histogram();
+    if actual_histogram != *expected_histogram {
+        return Err(InputError::Histogram {
+            expected: expected_histogram.clone(),
+            actual: actual_histogram,
+        });
     }
+
+    let actual_value = expr.eval().map_err(InputError::Eval)?;
+    if actual_value != *expected_value {
+        return Err(InputError::Value {
+            expected: expected_value.clone(),
+            actual: actual_value,
+        });
+    }
+
+    Ok(())
 }
 
 fn histogram(v: &[u32]) -> HashMap<u32, u32> {
